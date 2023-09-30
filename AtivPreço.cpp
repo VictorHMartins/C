638a029e2b6f0b107a4bba8c8198d44b6629de73
VictorHMartins <141377977+VictorHMartins@users.noreply.github.com>
2023-09-29 22:09:09 -0300

    Update and rename firmadopreço.cpp to AtivPreço.cpp

--- AtivPreço.cpp
+++ AtivPreço.cpp
@@ -2,15 +2,15 @@
 main()
 {float pre, prevista, preprazo;
 int op;
-printf("Digite p preÁo do produto");
+printf("Digite p pre√ßo do produto");
 scanf("%f", &pre);
-printf("Digite 1 para ‡ vista e 2 para ‡ prazo");
+printf("Digite 1 para √† vista e 2 para √† prazo");
 scanf("%i",&op);
 if(op== 1)
   {prevista- pre-(pre*10/100);
-  printf("O preÁo ‡ vista ser· %f", prevista);
+  printf("O pre√ßo √† vista ser√° %f", prevista);
   }
   else{preprazo = pre + pre*5/100;
-  printf("O preÁo a prazo È %f", preprazo);
+  printf("O pre√ßo a prazo √© %f", preprazo);
   }
 }
