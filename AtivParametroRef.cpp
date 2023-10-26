1f77f3cb8ece09044abc9c021fa72a3ea2be7a82
Victor Hugo <141377977+VictorHMartins@users.noreply.github.com>
2023-10-26 15:33:07 -0300

    Update AtivParametroRef.cpp

--- AtivParametroRef.cpp
+++ AtivParametroRef.cpp
@@ -1,28 +1,29 @@
-#include<conio.h>
-#include<stdio.h>
-#include<locale.h>
-#include<math.h>
+#include <conio.h>
+#include <stdio.h>
+#include <locale.h>
+#include <math.h>
 
-int somadref (int *a, int *b)
-    {int s;
-     printf("\na = %i e b = %i",*a,*b);
-     *a = 2*(*a);
-     *b = 2*(*b);
-     printf("\na = %i e b = %i",*a,*b);
-     s = *a + *b;
-     return s;
-    }
-    
+int somadref(int *a, int *b)
+{
+    int s;
+    printf("\na = %i e b = %i", *a, *b);
+    *a = 2 * (*a);
+    *b = 2 * (*b);
+    printf("\na = %i e b = %i", *a, *b);
+    s = *a + *b;
+    return s;
+}
+
 main()
-  {
-   setlocale(LC_ALL, "Portuguese");
-    int x,y,res;
-     printf("\nDigite o 1° número ");
-     scanf("%i",&x);
-     printf("\nDigite o 2° número ");
-     scanf("%i",&y);
-     res = somadref(&x,&y);
-     printf("\nDobro de x + dobro de y = %i",res);
-     printf("\n x = %i e y = %i",x,y);
-  }
+{
+    setlocale(LC_ALL, "Portuguese");
+    int x, y, res;
+    printf("\nDigite o 1Â° nÃºmero ");
+    scanf("%i", &x);
+    printf("\nDigite o 2Â° nÃºmero ");
+    scanf("%i", &y);
+    res = somadref(&x, &y);
+    printf("\nDobro de x + dobro de y = %i", res);
+    printf("\n x = %i e y = %i", x, y);
+}
 
